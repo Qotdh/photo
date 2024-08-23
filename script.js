771a5ff2ae06cfd5e086ae014cceea2463a2babe
dyaa <178745289+dyaa-edin@users.noreply.github.com>
2024-08-23 18:05:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+window.addEventListener('load', function() {
+    const images = document.images;
+    const totalImages = images.length;
+    let imagesLoaded = 0;
+
+    // التحقق من تحميل كل صورة
+    function imageLoaded() {
+        imagesLoaded++;
+        if (imagesLoaded === totalImages) {
+            document.getElementById('preloader').style.display = 'none';
+            document.documentElement.style.overflow = 'visible'; // إظهار محتوى الصفحة بعد التحميل
+            document.body.style.overflow = 'visible'; 
+        }
+    }
+
+    // إذا لم يكن هناك صور في الصفحة
+    if (totalImages === 0) {
+        imageLoaded();
+    } else {
+        // ربط حدث التحميل بكل صورة
+        for (let i = 0; i < totalImages; i++) {
+            if (images[i].complete) {
+                imageLoaded();
+            } else {
+                images[i].addEventListener('load', imageLoaded);
+                images[i].addEventListener('error', imageLoaded); // في حالة خطأ التحميل
+            }
+        }
+    }
+});
+
 $(document).ready(function(){
     $("img").click(function(){
         var t = $(this).attr("src");
